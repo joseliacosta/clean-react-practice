b1d4ab423284e3e7dcd82b561911ece48a29053d
Josélia L. Costa <joselialcosta@gmail.com>
2021-04-02 23:40:34 -0300

    refactor: creates a file to keep mocks and use it for http-client

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,15 +1,9 @@
-import {HttpPostClient} from 'data/protocols/http/http-post-client'
+import { HttpPostClientSpy } from '../../test/mock-http-client'
 import {RemoteAuthentication} from './remote-authentication'
 
 describe('Remote Authentication', () => {
   it('should call httpClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-      async post(url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
+
     const url = 'login_url'
     const httpPostClient = new HttpPostClientSpy()
     const sut = new RemoteAuthentication(url, httpPostClient)
