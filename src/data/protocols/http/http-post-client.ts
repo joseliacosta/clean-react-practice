5ff31d786c174c015abed76794b5288c585314ca
Josélia L. Costa <joselialcosta@gmail.com>
2021-04-03 18:50:50 -0300

    feat: throws InvalidCredentialsError when HttpPostClient receive a 401

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,7 +1,8 @@
+import {HttpResponse} from './http-response'
 export type HttpPostParams = {
   url: string
   body?: object
 }
 export interface HttpPostClient {
-  post(params: HttpPostParams): Promise<void>
+  post(params: HttpPostParams): Promise<HttpResponse>
 }
---
+++ src/data/protocols/http/http-response.ts
@@ -0,0 +1,8 @@
+export enum HttpStatusCode {
+  noContent = 204,
+  unauthorized = 401,
+}
+export type HttpResponse = {
+  statusCode: HttpStatusCode
+  body?: any
+}
